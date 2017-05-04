c0c064359f73add5f9bd40bae017b7c94920e278
elahevedadi <elahe.vedadi@yahoo.com>
2017-05-04 23:11:44 +0430

    14_ordibehesht

--- make_background_for_kmeans_weights.py
+++ make_background_for_kmeans_weights.py
@@ -2,7 +2,7 @@ import numpy
 import math
 import matplotlib.pyplot as plt
 
-set2_label = numpy.loadtxt("/Users/Apple/Desktop/first_chain_code_results/complete_weight_matrix_set2-3_examining/set3_no_back_traintest_not_normal_weight.txt")
+set2_label = numpy.loadtxt("/Users/Apple/Desktop/first_chain_code_results/complete_weight_matrix_set2-3_examining/kmeans_set3_test_train_notnormalw+2_w-4.txt")
 brain_index = numpy.loadtxt('/Users/Apple/Desktop/first_chain_code_results/complete_weight_matrix_set2-3_examining/brain_index.txt')
 
 labels_with_background = numpy.zeros((1817,4332))
@@ -15,12 +15,19 @@ for i in range(4332):
         labels_with_background[:,i] = set2_label[:,k]
         k=k+1
 
+    else:
+
+        labels_with_background[:,i] = -2
+        
+        
+            
+
         
         
 
 
 
-plt.imshow( labels_with_background),plt.colorbar(),plt.show()       
+#plt.imshow( labels_with_background),plt.colorbar(),plt.show()       
 
-open("original_set3_new_w+1_w-1.txt","w")
-numpy.savetxt("original_set3_new_w+1_w-1.txt",labels_with_background,"%.18e")
+open("back-2_set3_new_w+2_w-4.txt","w")
+numpy.savetxt("back-2_set3_new_w+2_w-4.txt",labels_with_background,"%.18e")
